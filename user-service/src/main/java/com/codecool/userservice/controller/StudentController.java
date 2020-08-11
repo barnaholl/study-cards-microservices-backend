9b884d902e28d0fa3ee33f1978b270dab8057fe5
Eszter Bokkon <bokkone@gmail.com>
2020-08-11 15:07:02 +0200

    Create StudentController

---
+++ user-service/src/main/java/com/codecool/userservice/controller/StudentController.java
@@ -0,0 +1,9 @@
+package com.codecool.userservice.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="${main.url}")
+public class StudentController {
+}
