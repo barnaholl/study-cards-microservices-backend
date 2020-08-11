87919aa9158a2409acfad65038f3469e49ce51fd
Barnuss09 <hollbarna@gmail.com>
2020-08-11 16:22:41 +0200

    get deck/all/userId

--- deck-handler-service/src/main/java/com/codecool/deckhandlerservice/controller/DeckController.java
+++ deck-handler-service/src/main/java/com/codecool/deckhandlerservice/controller/DeckController.java
@@ -2,9 +2,7 @@ package com.codecool.deckhandlerservice.controller;
 
 import com.codecool.deckhandlerservice.entity.Deck;
 import com.codecool.deckhandlerservice.repository.DeckRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,10 @@ public class DeckController {
     @GetMapping("/deck/all")
     public List<Deck> getAllDecks(){
         return deckRepository.findAll();
+    }
+
+    @GetMapping("/deck/all/{userId}")
+    public List<Deck> getAllDecksByUserId(@PathVariable("userId") Long userId){
+        return deckRepository.findAllByUserId(userId);
     }
 }
--- deck-handler-service/src/main/java/com/codecool/deckhandlerservice/repository/DeckRepository.java
+++ deck-handler-service/src/main/java/com/codecool/deckhandlerservice/repository/DeckRepository.java
@@ -3,5 +3,8 @@ package com.codecool.deckhandlerservice.repository;
 import com.codecool.deckhandlerservice.entity.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DeckRepository extends JpaRepository<Deck,Long> {
+    List<Deck> findAllByUserId(Long id);
 }
