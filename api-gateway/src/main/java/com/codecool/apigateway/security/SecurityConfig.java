13cada51bcbc36d362487c934f110ff37d088cbb
Eszter Bokkon <bokkone@gmail.com>
2020-08-11 18:05:20 +0200

    Create SecurityConfig base

---
+++ api-gateway/src/main/java/com/codecool/apigateway/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.codecool.apigateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/login").permitAll()
+                .antMatchers("/auth/register").permitAll()
+                .antMatchers("/**").permitAll()
+//                .antMatchers(HttpMethod.GET, "/**").authenticated()
+//                .antMatchers(HttpMethod.POST, "/**").authenticated()
+                .anyRequest().denyAll();
+    }
+}
