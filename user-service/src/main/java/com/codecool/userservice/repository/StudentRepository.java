d59b6bca7e3747a752a2946a972117520cbe1b77
Eszter Bokkon <bokkone@gmail.com>
2020-08-12 10:32:38 +0200

    Implement getUserByUsername and register methods in StudentService

--- user-service/src/main/java/com/codecool/userservice/repository/StudentRepository.java
+++ user-service/src/main/java/com/codecool/userservice/repository/StudentRepository.java
@@ -3,5 +3,9 @@ package com.codecool.userservice.repository;
 import com.codecool.userservice.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByUsername(String username);
 }
