997bfef5055ef0495a6ffc1b395879c5c3500cd4
Eszter Bokkon <bokkone@gmail.com>
2020-08-12 15:28:30 +0200

    Implement findByUsername and registerNewStudent methods

--- api-gateway/src/main/java/com/codecool/apigateway/service/StudentService.java
+++ api-gateway/src/main/java/com/codecool/apigateway/service/StudentService.java
@@ -1,10 +1,29 @@
 package com.codecool.apigateway.service;
 
+import com.codecool.apigateway.model.Student;
+import com.codecool.apigateway.model.UserCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class StudentService {
 
+    private final RestTemplate restTemplate;
 
+    @Value("${user-service.url}")
+    private String baseUrl;
 
+    public StudentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Student findByUsername(String username) throws UsernameNotFoundException {
+        return restTemplate.getForEntity(baseUrl + "/user/" + username, Student.class).getBody();
+    }
+
+    public void registerNewStudent(UserCredentials student) {
+        restTemplate.postForEntity(baseUrl + "/register", student, String.class);
+    }
 }
