a856221cd30a75420d0a77224213e8e95f9a29fd
Eszter Bokkon <bokkone@gmail.com>
2020-08-13 15:48:45 +0200

    Modify UserDetailsServiceImpl

--- api-gateway/src/main/java/com/codecool/apigateway/security/UserDetailsServiceImpl.java
+++ api-gateway/src/main/java/com/codecool/apigateway/security/UserDetailsServiceImpl.java
@@ -1,35 +1,40 @@
 package com.codecool.apigateway.security;
 
 import com.codecool.apigateway.model.Student;
-import com.codecool.apigateway.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.transaction.Transactional;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final StudentService studentService;
+    private final RestTemplate template;
 
-    /**
-     * Loads the user from the DB and converts it to Spring Security's internal User object.
-     * Spring will call this code to retrieve a user upon login from the DB.
-     */
+    @Value("${userservice.url}")
+    private String baseUrl;
+
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Student student = studentService.findByUsername(username);
-        if (student == null) {
-            throw new UsernameNotFoundException("Username not found");
-        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<>(username, headers);
+        Student student = template.exchange(baseUrl+"/"+username, HttpMethod.GET, entity, Student.class, username).getBody();
+
         return new User(
                 student.getUsername(),
                 student.getPassword(),
@@ -39,28 +44,5 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                         .collect(Collectors.toList())
         );
     }
-
-//    private final RestTemplate template;
-//
-//    @Value("${user-service.url}")
-//    private String baseUrl;
-//
-//    @Override
-//    @Transactional
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//
-//        Map<String, String> uriVariables = new HashMap<>();
-//        uriVariables.put("username", username);
-////        Student student = template.exchange(baseUrl+"/"+username, HttpMethod.GET, username, UserDetails.class, uriVariables);
-//
-//        return new User(
-//                student.getUsername(),
-//                student.getPassword(),
-//                student.getRoles()
-//                        .stream()
-//                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-//                        .collect(Collectors.toList())
-//        );
-//    }
 
 }
